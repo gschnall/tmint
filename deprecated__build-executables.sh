8b0172121cbcb79eeade37fc5713459c0334b717
gabe schnall <gabriel.schnall@gmail.com>
2020-12-11 00:52:38 -0800

    Add goreleases

---
+++ deprecated__build-executables.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# ----------------------
+# Use goreleases instead
+# ----------------------
+
+# To see all supported buids
+# > go tool dist list
+
+package=$1
+if [[ -z "$package" ]]; then
+  echo "usage: $0 <package-name>"
+  exit 1
+fi
+
+platforms=("windows/amd64" "windows/386" "darwin/amd64", "linux/amd64")
+
+for platform in "${platforms[@]}"
+do
+  platform_split=(${platform//\// })
+  GOOS=${platform_split[0]}
+  GOARCH=${platform_split[1]}
+  output_name='tmint-'$GOOS'-'$GOARCH
+  if [ $GOOS = "windows" ]; then
+    output_name+='.exe'
+  fi
+
+  env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+  if [ $? -ne 0 ]; then
+    echo 'An error has occurred! Aborting the script execution...'
+    exit 1
+  fi
+done
